b2386b5d9d6778cad12897057c270b0a60cafeb3
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-02-21 12:34:58 +0530

    Add files via upload

---
+++ Lesson/Day-03 (21-02-2024)/Pyramid.java
@@ -0,0 +1,19 @@
+package com.pattern;
+
+public class Pyramid {
+	public static void main() {
+		
+	}
+
+	public static void main(String[] args) {
+		int spaces = 40;
+		int stars = 1;
+		
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=stars; j++)  System.out.print("*");
+			System.out.println();
+			spaces--; stars+=2;
+		}
+	}
+}
