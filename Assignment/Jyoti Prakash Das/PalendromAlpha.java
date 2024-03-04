bf07384d38dbc94df17c0f6e52ff3b236101bb9a
JYOTIPRAKASH DAS <160457620+dasjyoti1@users.noreply.github.com>
2024-03-04 15:10:05 +0530

    Add files via upload

---
+++ Assignment/Jyoti Prakash Das/PalendromAlpha.java
@@ -0,0 +1,19 @@
+package com.pattern;
+
+public class PalendromAlpha {
+
+		public static void main(String[] args) {
+			int spaces = 40;
+			int alpha = 97;
+			
+			for(int i=0; i<10; i++) {
+				for(int j=1; j<=spaces; j++) System.out.print(" ");
+				for(alpha=97; alpha<=(97+i); alpha++) System.out.print((char)alpha);
+				for(alpha=alpha-2; alpha>=97; alpha--) System.out.print((char)alpha);
+				
+				System.out.println();
+				spaces--;
+			}
+			
+		}
+}
