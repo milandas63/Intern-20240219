d0c3dd1b5c498909c723a9da835843b9955ef23d
JYOTIPRAKASH DAS <160457620+dasjyoti1@users.noreply.github.com>
2024-03-05 14:42:33 +0530

    Add files via upload

---
+++ Assignment/Jyoti Prakash Das/Pyramid.java
@@ -0,0 +1,24 @@
+public class Pyramid {
+			public static void main() {
+
+			public static void main(String[] args) {
+				
+				int spaces = 40;
+				
+				int stars = 1;
+				
+				for(int i=1; i<=10; i++) {
+					
+					for(int j=1; j<=spaces; j++) System.out.print(" ");
+					
+					for(int j=1; j<=stars; j++)  System.out.print("a");
+					
+					System.out.println();
+					
+					spaces--; stars+=2;
+				}
+			}
+
+	}
+
+}
